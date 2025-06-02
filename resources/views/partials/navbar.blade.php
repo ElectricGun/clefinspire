61806a54c97e93fa1e5b0288d9652d925f677622
EV <>
2025-06-02 20:59:14 +0700

    navbar on home page

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,37 @@
+<style>
+    .no-outline:focus{
+        border-style: none !important;
+    }
+</style>
+
+<div class="container"> 
+    <div class="row mt-4 d-flex justify-content-between">
+        <div class="col-4">
+            <h2 class="fw-bold">Home</h2>
+        </div>
+        <div class="col-3 py-4 px-0">
+            <div class="input-group">
+                <i class="bi bi-search"></i>
+                <input type="email" class="form-control border-0 border-bottom border-5 rounded-0 py-0 shadow-none" id="search" placeholder="Search">
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="row d-flex justify-content-end">
+                <div class = "col-7 text-end">
+                    <h5>Beginner</h5>
+                    <h5 class="text-muted">Level 4</h5>
+                </div>
+                <div class="col-5 d-flex align-items-center">
+                    <div class="card ratio ratio-1x1 rounded-circle overflow-hidden" style="max-height: 75px; max-width: 75px;">
+                      <img 
+                        src="https://fastly.picsum.photos/id/780/200/200.jpg?hmac=2eohvtsHnYuBD7aalSWH4h6L6EKelvoFD9TV0Ax1K2A" 
+                        class="img-fluid w-100 h-100 object-fit-cover" 
+                        alt="Profile image">
+                    </div>
+                  </div>                  
+            </div>
+        </div>
+    </div>
+</div>
+
+<hr class="m-0">
