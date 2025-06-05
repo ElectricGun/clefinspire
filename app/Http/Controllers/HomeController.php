c2c07bca07514eec005f0a52754a16566e43ab25
EV <>
2025-06-05 13:57:54 +0700

    query builder args

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -34,7 +34,7 @@ class HomeController extends Controller
         $userlessons = DB::select(
             "
             SELECT l.title, SUM(uqs.is_completed) / COUNT(uqs.is_completed) as progress from UserQuestionStatus uqs 
-            JOIN UserCourse uc ON uc.user_id = 1 
+            JOIN UserCourse uc ON uc.user_id = ?
             JOIN UserLessonStatus uls ON uls.user_id = uc.user_id AND uls.course_id = uc.course_id
             JOIN Lesson l on uls.lesson_id = l.lesson_id
             WHERE uqs.task_id = (
@@ -42,20 +42,22 @@ class HomeController extends Controller
                 WHERE uts.lesson_id = (
                     SELECT uls.lesson_id  FROM UserCourse uc 
                     JOIN UserLessonStatus uls ON uls.user_id = uc.user_id AND uls.course_id = uc.course_id
-                    WHERE uc.user_id = 1
+                    WHERE uc.user_id = ?
                 )
                 AND uts.is_completed = 0
             )
             GROUP BY l.title, uqs.task_id
             "
+            ,[1, 1]
         );
+
 
         // foreach($userlessons as $e) {
         //     echo $e->title . " " . $e->progress;
         // }
 
         
-        return view('home', [-
+        return view('home', [
             'user' => $user, 'userlessons' => $userlessons
         ]);
     }
