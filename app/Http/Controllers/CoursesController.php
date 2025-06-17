fc64ebc85375bbc022a09c227f29a3d7dce13450
EV <_>
2025-06-17 15:59:00 +0700

    simplified CoursesController

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -8,8 +8,8 @@ use App\Providers\UserProfileProvider;
 
 class CoursesController extends Controller
 {
-    public function show_music_theory()
-    {
+
+    public static function show($coursetype, $coursetitle) {
         $user = ClefinspireAuth::get_user();
         $display_profile = UserProfileProvider::get_user_profile($user->last()->account_id);
 
@@ -23,7 +23,7 @@ class CoursesController extends Controller
                 left join Task t on t.lesson_id = l.lesson_id
                 left join UserLessonStatus uls on uls.course_id = uc.course_id
                 left join UserTaskStatus uts on uls.lesson_id = uts.course_id
-                where c.course_type = 'musictheory'
+                where c.course_type = '$coursetype'
                 group by c.course_type, c.difficulty, c.course_name, c.course_id
                 having count(t.task_id) > 0
                 ",
@@ -38,47 +38,20 @@ class CoursesController extends Controller
                     'user' => $user,
                     'courses' => $courses,
                     'display_profile' => $display_profile,
-                    'coursetype' => 'musictheory',
-                    'coursetitle' => 'Music Theory'
+                    'coursetype' => $coursetype,
+                    'coursetitle' => $coursetitle
                 ]
             );
         }
     }
 
+    public function show_music_theory()
+    {
+        return CoursesController::show('musictheory', 'Music Theory');
+    }
+
     public function show_ear_training()
     {
-        $user = ClefinspireAuth::get_user();
-        $display_profile = UserProfileProvider::get_user_profile($user->last()->account_id);
-        
-        foreach ($user as $u) {
-            $courses = DB::select(
-                "
-                select c.difficulty, c.course_name, c.course_id, coalesce(sum(uts.is_completed) / count(t.task_id), 0) as progress
-                from Course c 
-                left join UserCourse uc on c.course_id = uc.course_id and user_id = ?
-                left join Lesson l on l.course_id = c.course_id
-                left join Task t on t.lesson_id = l.lesson_id
-                left join UserLessonStatus uls on uls.course_id = uc.course_id
-                left join UserTaskStatus uts on uls.lesson_id = uts.course_id
-                where c.course_type = 'eartraining'
-                group by c.course_type, c.difficulty, c.course_name, c.course_id
-                having count(t.task_id) > 0
-                ",
-                [
-                    $u->user_id
-                ]
-            );
-
-            return view(
-                'courses',
-                [
-                    'user' => $user,
-                    'courses' => $courses,
-                    'display_profile' => $display_profile,
-                    'coursetype' => 'eartraining',
-                    'coursetitle' => 'Ear Training'
-                ]
-            );
-        }
+        return CoursesController::show('eartraining', 'Ear Training');
     }
 }
